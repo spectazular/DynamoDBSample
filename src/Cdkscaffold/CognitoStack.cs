32ab95d9c1b5c405cbb2f3a59aec2d482e0db961
Taz Hussein <tasleem.hussein@gmail.com>
2021-10-17 12:23:22 +1300

    Patial CDK stack for Cognito authorizer

---
+++ src/Cdkscaffold/CognitoStack.cs
@@ -0,0 +1,36 @@
+﻿using Amazon.CDK;
+using Amazon.CDK.AWS.Cognito;
+
+
+namespace Cdkscaffold
+{
+    public class CognitoStack : Stack
+    {
+        //TODO: Finish Cognito Athorizer stack: https://dev.to/ara225/adding-a-cognito-authorizer-to-api-gateway-with-the-aws-cdk-1ja2
+        public CognitoStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            UserPool userPool = new UserPool(this, "CDKCognitoStack", new UserPoolProps
+            {
+                UserPoolName = "Music-API-UserPool",
+                SelfSignUpEnabled = true,
+                SignInAliases = new SignInAliases { Email = true },
+                AutoVerify = new AutoVerifiedAttrs { Email = true },
+                StandardAttributes = new StandardAttributes 
+                { 
+                    GivenName = new StandardAttribute { Required = true, Mutable = true },
+                    FamilyName = new StandardAttribute { Required = true, Mutable = true }
+                },
+                PasswordPolicy = new PasswordPolicy 
+                { 
+                    MinLength = 6,
+                    RequireLowercase = true,
+                    RequireDigits = true,
+                    RequireUppercase = true,
+                    RequireSymbols = true
+                },
+                AccountRecovery = AccountRecovery.EMAIL_ONLY,
+                RemovalPolicy = RemovalPolicy.DESTROY
+            });
+        }
+    }
+}
